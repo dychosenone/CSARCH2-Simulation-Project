fadabffc570c7bc5d4e73eb408aab9d7e692c248
dychosenone <jacobmigueldy@gmail.com>
2022-02-04 16:11:08 +0800

    converter and other jquery functions

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 // This function runs when the site is loaded
-$(document).ready(function() {
+$(document).ready(function(e) {
+
+    $("#inputForm").submit(function (e) {
+        e.preventDefault();
+        var input = $("#input").val();
+
+        if(input == "") {
+            
+        }
+
+        console.log(input);
+        var converted = convertHexToBinary(input);
     
+        $("#finalAnswer").text(converted);
+    
+    });
 });
+
+
+
+
+function convertHexToBinary (number) {
+    var binary = (parseInt(number, 16).toString(2)).padStart(8, '0');
+    console.log(binary);
+    return binary;
+}
